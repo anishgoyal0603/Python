a5d2577bbc8c03867c89211d1689f1f0bd5417b7
anishgoyal0603 <anishgoyal0603@gmail.com>
2026-04-20 14:16:37 +0000

    data-types-commit

---
+++ Basics/data_types.py
@@ -0,0 +1,13 @@
+a = 1
+b = 1.5
+#c = none      # NameError: name 'none' is not defined. Did you mean: 'None'?   
+c = None
+d = True
+e = False
+f = 'a'
+print(type(a)) # <class 'int'>
+print(type(b)) # <class 'float'>
+print(type(c)) # <class 'NoneType'>
+print(type(d)) # <class 'bool'>
+print(type(e)) # <class 'bool'>
+print(type(f)) # <class 'str'>
