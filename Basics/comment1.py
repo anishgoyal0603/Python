1eebc82f60d709d73c2fbacd44f6f762ab1ad6bb
anishgoyal0603 <anishgoyal0603@gmail.com>
2026-04-20 14:17:14 +0000

    comment-commit

---
+++ Basics/comment1.py
@@ -0,0 +1,21 @@
+# Our First Program
+
+'''
+This is a 
+multi-line
+comment
+'''
+
+# ''  
+# dhari wala # error
+
+# ''
+
+# ""
+# dhari wala # error
+# ""
+
+"""
+dhari wala      , valid multi line comment
+"""
+
