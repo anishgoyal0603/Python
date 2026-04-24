d888db284c5a6464e10b97bdd6275dd16d6090b0
anishgoyal0603 <anishgoyal0603@gmail.com>
2026-04-24 18:39:11 +0000

    input-commit

---
+++ Basics/input_1.py
@@ -0,0 +1,16 @@
+# sum of 2 nums
+
+a = input("Enter value of a: ")
+b = input("Enter value of b: ")
+
+print(a+b)
+
+a = int(input("Enter value of a: "))
+b = int(input("Enter value of b: "))
+
+print(a+b)
+
+a = float(input("Enter value of a: "))
+b = float(input("Enter value of b: "))
+
+print(a+b)
