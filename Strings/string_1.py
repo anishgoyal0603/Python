30c4bc616594d013da6bf346c77fc2c82414c2a5
anishgoyal0603 <anishgoyal0603@gmail.com>
2026-05-08 03:59:14 +0000

    string-commit

---
+++ Strings/string_1.py
@@ -0,0 +1,10 @@
+# strings are immutable in python
+
+str1 = 'oekkd' # valid
+str2 = "efimidmkwd" # valid
+str3 = ''' dhari1 ''' # valid
+str4 = """ dhari2 """ # valid
+
+print(str1,str2,str3,str4)
+print(len(str1),len(str2),len(str3),len(str4))
+
