7f3bff222296285a77aa98db14cbb5c361512626
anishgoyal0603 <anishgoyal0603@gmail.com>
2026-05-09 16:57:48 +0000

    slicing-commit

---
+++ Strings/slicing_1.py
@@ -0,0 +1,8 @@
+word1 = "Python"
+
+#print(word1[]) # error
+#print(word1(:)) # error
+print(word1[:]) # valid , output = Python
+print(word1[::]) # valid , output = Python
+print(word1[2:])
+print(word1[-4:-2]) # th
