06d442b7b9973e16afe5958ac24deb1ed7bb30e4
anishgoyal0603 <anishgoyal0603@gmail.com>
2026-05-09 16:58:24 +0000

    f-string-commit

---
+++ Strings/f_string_1.py
@@ -0,0 +1,12 @@
+a = 5
+b = 10
+
+print(f"sum of {a} and {b} is {a+b}") # valid
+print(F"sum of {a} and {b} is {a+b}") # valid
+print(f'avg of {a} and {b} is {(a+b/2)}')
+print(F'avg of {a} and {b} is {(a+b/2)}')
+print(F'''avg of {a} and {b} is {(a+b/2)}''')
+print(f'''avg of {a} and {b} is {(a+b/2)}''')
+print(F"""avg of {a} and {b} is {(a+b/2)}""")
+print(f"""avg of {a} and {b} is {(a+b/2)}""")
+ 
