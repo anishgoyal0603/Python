2f1a7f5a10fc85866626d3e22c19fe7a66bd917d
anishgoyal0603 <anishgoyal0603@gmail.com>
2026-05-10 11:56:56 +0000

    list-commit-1

---
+++ Lists/list_1.py
@@ -0,0 +1,8 @@
+marks = [99 , 89 , 100 , 65 , 92]
+
+print(marks)
+print(len(marks))
+#print(marks(3)) # TypeError: 'list' object is not callable
+print(marks[3])
+print(marks[len(marks)-1])
+#print(marks[len(marks)]) # IndexError: list index out of range
