4c46e441f2204d3837e641738dbed23de4a50713
anishgoyal0603 <anishgoyal0603@gmail.com>
2026-05-11 06:26:17 +0000

    list-commit

---
+++ Lists/list_method_1.py
@@ -0,0 +1,6 @@
+li1 = [3,7,2]
+
+print(li1)
+#print(li1.append()) # TypeError: list.append() takes exactly one argument (0 given)
+print(li1.append(1))
+#print(li1.append(2,5)) # TypeError: list.append() takes exactly one argument (2 given)
