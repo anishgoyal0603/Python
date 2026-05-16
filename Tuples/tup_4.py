668e801bf5b00d90a8dddd16962ca49730dd1b7b
anishgoyal0603 <anishgoyal0603@gmail.com>
2026-05-16 18:01:58 +0000

    tuple-commit

---
+++ Tuples/tup_4.py
@@ -0,0 +1,7 @@
+tup1 = (1,2,3,4,5)
+
+#print(tup1[]) # SyntaxError: invalid syntax. Perhaps you forgot a comma?
+print(tup1[:]) # (1, 2, 3, 4, 5)
+print(tup1[::]) # (1, 2, 3, 4, 5)
+print(tup1[0:3]) # (1, 2, 3)
+
