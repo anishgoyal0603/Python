e8ea89ce62947aa7fd083c5a346128d82b40d59a
anishgoyal0603 <anishgoyal0603@gmail.com>
2026-05-16 18:01:31 +0000

    tuple-commit

---
+++ Tuples/tup_3.py
@@ -0,0 +1,15 @@
+tup1 = (8.4)
+tup2 = (7)
+tup3 = ('onr')
+
+print(tup1)
+print(type (tup1)) # <class 'float'>
+print(tup2)
+print(type (tup2)) # <class 'int'>
+print(tup3)
+print(type (tup3)) # <class 'str'>
+
+tup4 = (7 , )
+
+print(tup4)
+print(type(tup4))
