81c75ef97c067cb5072457bb10e4ab9e27a5f3a6
anishgoyal0603 <anishgoyal0603@gmail.com>
2026-05-24 16:28:40 +0000

    init-commit

---
+++ OOPS/init_2.py
@@ -0,0 +1,14 @@
+class Student:
+    def __init__(self,name):
+        self.name = name
+
+stu1 = Student("Rahul")
+stu2 = Student("Urvashi")
+stu3 = Student("Shradha")   
+
+print(stu1.name)
+print(stu2.name)
+print(stu3.name)
+
+
+
